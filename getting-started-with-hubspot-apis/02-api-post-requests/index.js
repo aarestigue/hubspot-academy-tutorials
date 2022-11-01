f9e7b19ce85fd7584d59b7f031fd4c961b939a16
aarestigue <108528013+aarestigue@users.noreply.github.com>
2022-11-01 21:01:41 +0000

    notes added

--- getting-started-with-hubspot-apis/02-api-post-requests/index.js
+++ getting-started-with-hubspot-apis/02-api-post-requests/index.js
@@ -1,6 +1,8 @@
+/* 1. Import Express and axios */
 const express = require('express');
 const axios = require('axios');
 
+/* 2. Create an express app */
 const app = express();
 
 app.set('view engine', 'pug');
@@ -10,17 +12,26 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PRIVATE_APP_ACCESS = "";
 
+const PRIVATE_APP_ACCESS = "pat-eu1-3d38a751-f8a3-4112-97a2-3c5400640c54";
+
+/* 5. Start with routes: 
+a. create a variable for inpoint url
+b. Set a variable for headers (Auth & content type - where the app variable created
+    is set with type)
+*/
 app.get('/contacts', async (req, res) => {
 
     const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    
+    
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
 
     try {
+        
         const resp = await axios.get(contacts, { headers });
         const data = resp.data.results;
         res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
@@ -34,7 +45,7 @@ app.get('/update', async (req, res) => {
     // http://localhost:3000/update?email=rick@crowbars.net
     const email = req.query.email;
 
-    const getContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email&properties=email,favorite_book`;
+    const getContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email&properties=email,favorite_books`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
@@ -45,7 +56,7 @@ app.get('/update', async (req, res) => {
         const data = response.data;
 
         // res.json(data);
-        res.render('update', {userEmail: data.properties.email, favoriteBook: data.properties.favorite_book});
+        res.render('update', {userEmail: data.properties.email, favoriteBook: data.properties.favorite_books});
         
     } catch(err) {
         console.error(err);
@@ -55,7 +66,7 @@ app.get('/update', async (req, res) => {
 app.post('/update', async (req, res) => {
     const update = {
         properties: {
-            "favorite_book": req.body.newVal
+            "favorite_books": req.body.newVal
         }
     }
 
